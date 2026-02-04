7d310939759bb216b339ad8c920dec418f7e24dd
Victor Nunez <tu_correo@gmail.com>
2026-02-03 23:43:32 -0300

    refactor: agregar uso de sockets para conexiones locales

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from app import create_app
+import socket
 
 app = create_app()
 
 if __name__ == '__main__':
     # debug=True permite ver errores en vivo (Crucial para principiantes)
-    app.run(debug=True)
+    host_name = socket.gethostname()
+    ip = socket.gethostbyname(host_name) 
+    app.run(host=ip, debug=True)
